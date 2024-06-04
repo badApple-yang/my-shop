8455cf8119ca3e15237c6674d39ffa6cfc5f6a24
among-the-people <000720aa>
2024-06-04 23:22:50 +0800

    "创建tabbar"

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-
+  <router-view></router-view>
+  <tab-bar v-if="isShowTabbar"></tab-bar>
 </template>
-
 <script setup>
-
+import TabBar from "./components/TabBar.vue";
+import { useRoute } from "vue-router";
+import { ref, watch } from "vue";
+const route = useRoute();
+const isShowTabbar = ref(true);
+// 监听路由中的 isTab 是否为 true，如果为 true，展示底部 TabBar
+watch(
+  () => route.meta,
+  (val) => {
+    isShowTabbar.value = val.isTab;
+  }
+);
 </script>
+<style>
+#app {
+  color: #2c3e50;
+  line-height: 24px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import Vant from "vant";
+import "vant/lib/index.css";
+import router from "./router";
+const app = createApp(App);
+app.use(Vant);
+app.use(router);
+app.mount("#app");
